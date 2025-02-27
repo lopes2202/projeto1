8aca742bdab02bf72b5bafdb78df02232a95d212
unknown <2314290121@ACADEMICO.IESB.BR>
2025-02-27 09:05:42 -0300

    teste

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,4 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
+import { Image, StyleSheet, View, Text } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -11,45 +10,54 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require('@/assets/images/default-banner.jpg')}
+          style={styles.imagemLogo}
+          resizeMode="cover" 
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Bem Vindo!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="subtitle">Objeto</ThemedText>
+        <ThemedView style={styles.imageContainer}>
+          <Image
+            source={require('@/assets/images/fusca1.jpg')} 
+            style={styles.reactLogo}
+            resizeMode="contain" 
+          />
+          <Text style={styles.textInfo}>
+            VW - Fusca{"\n"}
+            Ano: 1978{"\n"}
+            Cor: Preto
+          </Text>
+        </ThemedView>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
+      <Image
+            source={require('@/assets/images/celta1.jpg')} 
+            style={styles.reactLogo}
+            resizeMode="contain" 
+          />
+          <Text style={styles.textInfo}>
+          CHEVY - Celta{"\n"}
+          Ano: 2000{"\n"}
+          Cor: Vermelho
+          </Text>      
+          </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      <Image
+            source={require('@/assets/images/seal.jpg')} 
+            style={styles.reactLogo}
+            resizeMode="contain" 
+          />
+          <Text style={styles.textInfo}>
+          BYD - Seal{"\n"}
+          Ano: 2022{"\n"}
+          Cor: Prata
+          </Text>            
+          </ThemedView>
     </ParallaxScrollView>
   );
 }
@@ -65,10 +73,21 @@ const styles = StyleSheet.create({
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    height: 300, 
+    width: '100%', 
+    marginBottom: 10, 
   },
+  imagemLogo: {
+    height: 250,
+    width: '100%', 
+    position: 'relative', 
+  },
+  imageContainer: {
+    marginBottom: 15,
+  },
+  textInfo: {
+    marginTop: 10,
+    fontSize: 18,
+    color: 'gray', 
+  }
 });
